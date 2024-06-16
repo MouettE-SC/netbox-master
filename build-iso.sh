9172de8bdd90caeff4832963cf81309be5d9d407
Charles Leclerc <leclerc.charles@gmail.com>
2024-06-16 16:04:32 +0200

    Initial ISO build

---
+++ build-iso.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eo pipefail
+
+mkdir -p custom
+sha256sum -c AlmaLinux-8.10-x86_64-boot.iso.sha256
+7z x -y -ocustom AlmaLinux-8.10-x86_64-boot.iso
+rm -rf 'custom/[BOOT]'
+cp -f netbox-ks.cfg custom
+cp -f grub.cfg custom/EFI/BOOT
+git describe --all --long --dirty > custom/iso.build
+( cd custom ; mkisofs -o ../AlmaLinux-8.10-x86_64-boot-netbox.iso -b isolinux/isolinux.bin -J -R -l \
+                      -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table \
+                      -eltorito-alt-boot -eltorito-platform efi -eltorito-boot images/efiboot.img -no-emul-boot \
+                      -graft-points -joliet-long -V "AlmaLinux-8-10-x86_64-dvd" . )
+rm -rf custom
